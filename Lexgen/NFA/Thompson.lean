89cd6e2f8d3f8b02c280f3e5e742b6577d13878b
oleja <shabanovoleg04@gmail.com>
2026-09-12 23:56:19 +0300

    feat(NFA): implement Thompson's construction

---
+++ Lexgen/NFA/Thompson.lean
@@ -0,0 +1,62 @@
+import Lexgen.NFA.Automaton
+import Lexgen.Regex.Ast
+
+private def translate (startState : Nat) : RegularExprAST → NFA
+  | .ε =>
+    {
+      start  := startState,
+      trans  := [(startState, .ε, startState + 1)],
+      accept := startState + 1
+    }
+  | .symbol c =>
+    {
+      start  := startState,
+      trans  := [(startState, .char c, startState + 1)],
+      accept := startState + 1
+    }
+  | .dot =>
+    {
+      start  := startState,
+      trans  := [(startState, .dot, startState + 1)],
+      accept := startState + 1
+    }
+  | .concat first rest =>
+    let fstNFA := translate startState first
+    let sndNFA := translate fstNFA.accept rest
+    {
+      start  := fstNFA.start,
+      trans  := fstNFA.trans ++ sndNFA.trans,
+      accept := sndNFA.accept
+    }
+  | .repeated regex =>
+    let subNFA   := translate (startState + 1) regex
+    let endState := subNFA.accept + 1
+    {
+      start := startState,
+      trans :=
+        subNFA.trans ++
+        [ (startState, .ε, subNFA.start),
+          (subNFA.accept, .ε, endState),
+          (subNFA.accept, .ε, subNFA.start),
+          (startState, .ε, endState)
+        ],
+      accept := endState
+    }
+  | .alt left right =>
+    let leftNFA  := translate (startState + 1) left
+    let rightNFA := translate (leftNFA.accept + 1) right
+    let endState := rightNFA.accept + 1
+    {
+      start := startState,
+      trans :=
+        leftNFA.trans ++
+        rightNFA.trans ++
+        [ (startState, .ε, leftNFA.start),
+          (startState, .ε, rightNFA.start),
+          (leftNFA.accept, .ε, endState),
+          (rightNFA.accept, .ε, endState)
+        ],
+      accept := endState
+    }
+
+def reToNFA (regex : RegularExprAST) : NFA := translate 0 regex
