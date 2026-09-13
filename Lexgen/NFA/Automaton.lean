9ba7bb4c2cac88e8fb4c7ed99d0656e9a2884c59
oleja <shabanovoleg04@gmail.com>
2026-09-13 17:51:14 +0300

    docs(NFA): add docstrings for module and declarations

--- Lexgen/NFA/Automaton.lean
+++ Lexgen/NFA/Automaton.lean
@@ -1,11 +1,41 @@
+/-!
+# NFA
+
+Defines the NFA representation (`NFA`).
+-/
+
+/--
+The label type for NFA transitions.
+-/
 inductive Symbol where
-  | ε
-  | dot
+  /--
+  Matches the literal character `c`.
+  -/
   | char (c : Char)
+  /--
+  Matches any single character.
+  -/
+  | dot
+  /--
+  Matches the empty string.
+  -/
+  | ε
 deriving Repr
 
+/--
+The non-deterministic finite automaton representation.
+-/
 structure NFA where
+  /--
+  The start state.
+  -/
   start  : Nat
-  trans  : List (Nat × Symbol × Nat)
+  /--
+  The transitions table.
+  -/
+  trans  : List (Nat × Symbol × Nat) -- TODO: choose better table representation
+  /--
+  The accept state.
+  -/
   accept : Nat
 deriving Repr
