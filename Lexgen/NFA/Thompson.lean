865b0169d50ab2d3a08327cbb969aaf5c1aa71e9
oleja <shabanovoleg04@gmail.com>
2026-09-15 00:05:57 +0300

    refactor(thompson): adapt to state-based array `NFA` representation

--- Lexgen/NFA/Thompson.lean
+++ Lexgen/NFA/Thompson.lean
@@ -15,63 +15,43 @@ Internal implementation of Thompson's algorithm.
 -/
 private def translate (startState : Nat) : RegularExprAST → NFA
   | .ε =>
-    {
-      start  := startState,
-      trans  := [(startState, .ε, startState + 1)],
-      accept := startState + 1
-    }
+    { nodes := #[.ε (startState + 1)] }
   | .symbol c =>
-    {
-      start  := startState,
-      trans  := [(startState, .char c, startState + 1)],
-      accept := startState + 1
-    }
+    { nodes := #[.char c (startState + 1)] }
   | .dot =>
-    {
-      start  := startState,
-      trans  := [(startState, .dot, startState + 1)],
-      accept := startState + 1
-    }
+    { nodes := #[.dot (startState + 1)] }
   | .concat first rest =>
     let fstNFA := translate startState first
-    let sndNFA := translate fstNFA.accept rest
-    {
-      start  := fstNFA.start,
-      trans  := fstNFA.trans ++ sndNFA.trans,
-      accept := sndNFA.accept
-    }
+    let sndNFA := translate (startState + fstNFA.nodes.size) rest
+    { nodes := fstNFA.nodes ++ sndNFA.nodes }
   | .repeated regex =>
-    let subNFA   := translate (startState + 1) regex
-    let endState := subNFA.accept + 1
+    let subStart := startState + 1
+    let subNFA   := translate subStart regex
+    let endState := subStart + subNFA.nodes.size + 1
     {
-      start := startState,
-      trans :=
-        subNFA.trans ++
-        [ (startState, .ε, subNFA.start),
-          (subNFA.accept, .ε, endState),
-          (subNFA.accept, .ε, subNFA.start),
-          (startState, .ε, endState)
-        ],
-      accept := endState
+      nodes :=
+        #[.split subStart endState] ++
+        subNFA.nodes ++
+        #[.split subStart endState]
     }
   | .alt left right =>
-    let leftNFA  := translate (startState + 1) left
-    let rightNFA := translate (leftNFA.accept + 1) right
-    let endState := rightNFA.accept + 1
+    let leftStart  := startState + 1
+    let leftNFA    := translate leftStart left
+    let rightStart := leftStart + leftNFA.nodes.size + 1
+    let rightNFA   := translate rightStart right
+    let endState   := rightStart + rightNFA.nodes.size + 1
     {
-      start := startState,
-      trans :=
-        leftNFA.trans ++
-        rightNFA.trans ++
-        [ (startState, .ε, leftNFA.start),
-          (startState, .ε, rightNFA.start),
-          (leftNFA.accept, .ε, endState),
-          (rightNFA.accept, .ε, endState)
-        ],
-      accept := endState
+      nodes :=
+      #[.split leftStart rightStart] ++
+      leftNFA.nodes ++
+      #[.ε endState] ++
+      rightNFA.nodes ++
+      #[.ε endState]
     }
 
 /--
 Translates a `RegularExprAST` into an `NFA` using Thompson's construction.
 -/
-def reToNFA (regex : RegularExprAST) : NFA := translate 0 regex
+def reToNFA (regex : RegularExprAST) : NFA :=
+  let translated := translate 0 regex
+  { nodes := translated.nodes ++ #[.done] }
