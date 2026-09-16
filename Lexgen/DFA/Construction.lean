dbf6b0f4b3b35a2855dcbd48585d1a2798bf365f
oleja <shabanovoleg04@gmail.com>
2026-09-16 21:37:17 +0300

    refactor(NFA): change `εClosure` to accept `visited` explicitly

--- Lexgen/DFA/Construction.lean
+++ Lexgen/DFA/Construction.lean
@@ -6,21 +6,19 @@ import Lexgen.NFA.Automaton
 
 TODO: Prove termination. An `Array` may be needed instead of a `HashSet`.
 -/
-private partial def NFA.εClosure (nfa : NFA) (node : Nat) : Std.HashSet Nat :=
-  let rec closure (visited : Std.HashSet Nat) (node : Nat) : Std.HashSet Nat :=
-    if visited.contains node then
-      visited
-    else
-      -- TODO: Can we prove indexing?
-      let state   : Node            := nfa.nodes[node]!
-      let newVisited : Std.HashSet Nat := visited.insert node
-      match state with
-      | .done              => newVisited
-      | .edge (.char _) _          => newVisited
-      | .edge .dot _             => newVisited
-      | .edge .ε next            => closure newVisited next
-      -- When constructing the ε-closure for `next₂`,
-      -- the states in the ε-closure of `next₁`
-      -- have already been marked as visited.
-      | .split next₁ next₂ => closure (closure newVisited next₁) next₂
-  closure {} node
+private partial def NFA.εClosure (nfa : NFA) (visited : Std.HashSet Nat) (node : Nat) : Std.HashSet Nat :=
+  if visited.contains node then
+    visited
+  else
+    -- TODO: Can we prove indexing?
+    let state      : Node            := nfa.nodes[node]!
+    let newVisited : Std.HashSet Nat := visited.insert node
+    match state with
+    | .done              => newVisited
+    | .edge (.char _) _  => newVisited
+    | .edge .dot _       => newVisited
+    | .edge .ε next      => nfa.εClosure newVisited next
+    -- When constructing the ε-closure for `next₂`,
+    -- the states in the ε-closure of `next₁`
+    -- have already been marked as visited.
+    | .split next₁ next₂ => nfa.εClosure (nfa.εClosure newVisited next₁) next₂
